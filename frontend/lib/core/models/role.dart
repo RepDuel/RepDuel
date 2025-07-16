97373bc5f6b8cc06883c36e440ef62793342bae3
roywangswe <roywangswe@gmail.com>
2025-07-16 14:41:02 -0400

    add role and user_profile models

---
+++ frontend/lib/core/models/role.dart
@@ -0,0 +1,15 @@
+// frontend/lib/core/models/role.dart
+
+import 'package:flutter/material.dart';
+
+class Role {
+  final String name;
+  final Color color;
+  final String iconPath;
+
+  Role({
+    required this.name,
+    required this.color,
+    required this.iconPath,
+  });
+}
---
+++ frontend/lib/core/models/user_profile.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'role.dart';
+
+class UserProfile {
+  final String id;
+  final String username;
+  final String? avatarUrl;
+  final List<Role> roles;
+
+  UserProfile({
+    required this.id,
+    required this.username,
+    this.avatarUrl,
+    required this.roles,
+  });
+
+  Role get highestRole {
+    return roles.isNotEmpty
+        ? roles.first
+        : Role(name: 'Unranked', color: const Color(0xFFFFFFFF), iconPath: '');
+  }
+}
