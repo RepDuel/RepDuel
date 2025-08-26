bc62ec8dbc9efe3432c26e55c208835e262ad385
Roy Wang <roywangswe@gmail.com>
2025-08-26 07:44:06 -0400

    Update score model to delete cascade

--- backend/app/models/score.py
+++ backend/app/models/score.py
@@ -1,19 +1,15 @@
 # backend/app/models/score.py
-
 from datetime import datetime
-
 from sqlalchemy import Boolean, Column, DateTime, Float, ForeignKey, Integer, String
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
-
 from app.db.base_class import Base
-
 
 class Score(Base):
     __tablename__ = "scores"
-
+    
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     scenario_id = Column(String, ForeignKey("scenarios.id"), nullable=False)
     score_value = Column(Float, nullable=False)
     weight_lifted = Column(Float, nullable=False)
@@ -21,8 +17,7 @@ class Score(Base):
     sets = Column(Integer, nullable=True)
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
     is_bodyweight = Column(Boolean, default=False)
-
+    
     # --- Relationships ---
     scenario = relationship("Scenario", back_populates="scores")
     user = relationship("User", back_populates="scores")
-    
