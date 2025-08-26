0bee925b72afafbd759340bcbde4d53368bc3d29
Roy Wang <roywangswe@gmail.com>
2025-08-26 10:50:37 -0400

    add env vars to platform_redirects_mobile

--- frontend/lib/core/utils/platform_redirects_mobile.dart
+++ frontend/lib/core/utils/platform_redirects_mobile.dart
@@ -1,14 +1,10 @@
-// frontend/lib/core/utils/platform_redirects_mobile.dart
+import 'package:repduel/core/config/env.dart';
 
 class PlatformRedirects {
-  /// Returns a map containing the correct success/cancel URLs for mobile.
   static Map<String, String> get urls {
-    // This MUST be the public, custom domain URL of your live Flutter web app.
-    const String liveWebAppUrl = 'https://www.repduel.io';
-
     return {
-      'success_url': '$liveWebAppUrl/payment-success',
-      'cancel_url': '$liveWebAppUrl/payment-cancel',
+      'success_url': Env.stripeSuccessUrl,
+      'cancel_url': Env.stripeCancelUrl,
     };
   }
 }
