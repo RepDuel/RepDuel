f1b7e95c0a34808232cc3569d44e22b9e91fef11
Roy Wang <roywangswe@gmail.com>
2025-10-19 12:16:20 -0400

    Use Roboto on ios and web

--- frontend/lib/theme/app_theme.dart
+++ frontend/lib/theme/app_theme.dart
@@ -1,9 +1,13 @@
 // frontend/lib/theme/app_theme.dart
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   static ThemeData get lightTheme {
+    final baseTextTheme =
+        GoogleFonts.robotoTextTheme(ThemeData.light().textTheme);
+
     return ThemeData(
       brightness: Brightness.light,
       scaffoldBackgroundColor: Colors.grey[100],
@@ -12,27 +16,32 @@ class AppTheme {
         primary: Colors.blueAccent,
         secondary: Colors.indigoAccent,
       ),
-      appBarTheme: const AppBarTheme(
+      appBarTheme: AppBarTheme(
         backgroundColor: Colors.white,
         elevation: 1,
-        iconTheme: IconThemeData(color: Colors.black87),
-        titleTextStyle: TextStyle(
+        iconTheme: const IconThemeData(color: Colors.black87),
+        titleTextStyle: GoogleFonts.roboto(
           color: Colors.black87,
           fontSize: 20,
           fontWeight: FontWeight.bold,
         ),
+        toolbarTextStyle: GoogleFonts.roboto(
+          color: Colors.black87,
+          fontSize: 16,
+        ),
       ),
-      textTheme: const TextTheme(
-        bodyLarge: TextStyle(fontSize: 16.0),
-        bodyMedium: TextStyle(fontSize: 14.0),
-      ),
+      textTheme: baseTextTheme,
       inputDecorationTheme: const InputDecorationTheme(
         border: OutlineInputBorder(),
       ),
+      fontFamily: GoogleFonts.roboto().fontFamily,
     );
   }
 
   static ThemeData get darkTheme {
+    final baseTextTheme =
+        GoogleFonts.robotoTextTheme(ThemeData.dark().textTheme);
+
     return ThemeData(
       brightness: Brightness.dark,
       scaffoldBackgroundColor: const Color(0xFF1E1E1E),
@@ -41,23 +50,25 @@ class AppTheme {
         primary: Colors.tealAccent,
         secondary: Colors.cyanAccent,
       ),
-      appBarTheme: const AppBarTheme(
-        backgroundColor: Color(0xFF2C2C2C),
+      appBarTheme: AppBarTheme(
+        backgroundColor: const Color(0xFF2C2C2C),
         elevation: 1,
-        iconTheme: IconThemeData(color: Colors.white),
-        titleTextStyle: TextStyle(
+        iconTheme: const IconThemeData(color: Colors.white),
+        titleTextStyle: GoogleFonts.roboto(
           color: Colors.white,
           fontSize: 20,
           fontWeight: FontWeight.bold,
         ),
+        toolbarTextStyle: GoogleFonts.roboto(
+          color: Colors.white,
+          fontSize: 16,
+        ),
       ),
-      textTheme: const TextTheme(
-        bodyLarge: TextStyle(fontSize: 16.0),
-        bodyMedium: TextStyle(fontSize: 14.0),
-      ),
+      textTheme: baseTextTheme,
       inputDecorationTheme: const InputDecorationTheme(
         border: OutlineInputBorder(),
       ),
+      fontFamily: GoogleFonts.roboto().fontFamily,
     );
   }
 }
